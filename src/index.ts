914429e5b02d2484fa8a344aa6d472178c377f73
Oksnard <36508223+Oksnard@users.noreply.github.com>
2022-12-31 17:13:58 +0300

    init custom starting template

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import logger from 'morgan'
+
+// routes
+import streamController from "./modules/stream/stream.controller"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
+app.use('/stream', streamController)
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`stream is starting on port: ${PORT}`)
+})
